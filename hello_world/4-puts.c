35a0e39855a021c1c4547a89bb8fb86f8e0bb371
Hitch95 <bahloul.condorcet@gmail.com>
2022-09-30 03:31:52 -0700

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,12 +8,12 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 
-    char Mystr[] = "\"Programming is like building a multilingual puzzle";
-    
-    puts(Mystr);
-    
-    return 0;
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(Mystr);
+
+return 0;
 }
