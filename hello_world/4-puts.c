b43fe9c7e00dfb845163df2ac0d64c2eabe204b8
Hitch95 <bahloul.condorcet@gmail.com>
2022-09-30 03:29:11 -0700

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main()
 {
 
-    char Mystr[] = "Programming is like building a multilingual puzzle";
+    char Mystr[] = "\"Programming is like building a multilingual puzzle";
     
     puts(Mystr);
     
