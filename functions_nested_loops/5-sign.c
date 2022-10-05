968cd8d3abdec7f8839e0aeb7765ad57093cf202
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-05 06:23:18 -0700

    5. Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _print - Entry point
+ *@c: take the integer value of the character
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	 }
+	else if(n == 0)
+	 {
+		 _putchar('0');
+		 return (0);
+	 }
+	else
+	{
+	        _putchar('-');
+		return (-1);
+	}
+}
