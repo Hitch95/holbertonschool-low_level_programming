15df93d4b02b1c92bc5f4392d7d0e5c5140bc3e8
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-07 14:03:48 -0700

    8. You are so much sunshine in every square inch

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,13 +5,21 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size; j++)
+
+		for (i = 1; i <= size; i++)
 		{
+
+			for (j = 1; j <= size; j++)
+			{
 			_putchar(35);
+			}
+		_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
-
 }
