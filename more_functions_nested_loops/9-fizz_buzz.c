5578c6c2373a972b757a7f91a3ee5ed1f42d707c
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-07 06:08:22 -0700

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,22 @@ int main(void)
    print 'FizzBuzz' in place of the number
 */
         if (i % 15 == 0)
-            printf ("FizzBuzz\t");
+            printf ("FizzBuzz ");
 
 /**
    number divisible by 3, print 'Fizz' in place of the number
 */
         else if (( i % 3) == 0)
-            printf("Fizz\t");
+            printf("Fizz ");
 
 /**
    number divisible by 5, print 'Buzz' in place of the number
 */
         else if ((i % 5) == 0)
-            printf("Buzz\t");
+            printf("Buzz ");
 
         else
-            printf("%d\t", i);
+            printf("%d ", i);
     }
     return 0;
 }
