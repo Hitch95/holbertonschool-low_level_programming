860c18a48d986ece446a609d2aa14a2822ed1e91
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-07 06:20:16 -0700

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+
+/**
+* Description:  Resolution of FizzBuzz test;
+*/
+
 int main(void)
 {
     int i, n ;
@@ -9,27 +14,19 @@ int main(void)
     for (i = 1; i <= n; i++)
     {
 
-/**
-   number divisible by 3 and 5 will always be divisible by 15,
-   print 'FizzBuzz' in place of the number
-*/
         if (i % 15 == 0)
             printf ("FizzBuzz ");
 
-/**
-   number divisible by 3, print 'Fizz' in place of the number
-*/
+
         else if (( i % 3) == 0)
             printf("Fizz ");
 
-/**
-   number divisible by 5, print 'Buzz' in place of the number
-*/
         else if ((i % 5) == 0)
             printf("Buzz ");
 
         else
             printf("%d ", i);
     }
+    printf("\n");
     return 0;
 }
