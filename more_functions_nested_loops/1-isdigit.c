66c7f2f5e0e2c499c9eba3785d84d1408b728439
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-07 00:57:35 -0700

    1. isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code.
+ *@c: take an integer value
+ * Return: Always 1 if it is a Digit:
+ */
+
+int _isdigit(char c)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
