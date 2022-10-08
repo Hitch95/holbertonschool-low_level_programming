d2cd5a2b15cafc801144eba0408c64ddae7aa7cc
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-08 11:43:55 -0700

    10. a + b

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * add - check the code;
+ * @a: take an integer value;
+ * @b: take another integer value;
+ * Return: Always 0 (if success);
+ */
+
 int add(int a, int b)
 
 {
