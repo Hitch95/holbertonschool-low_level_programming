753ae4d31eac5f9b7c98021870f8b91930288fac
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-08 11:39:53 -0700

    2. 10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet_x10 - Entry point;
+ * Return: Always 0 (Success);
  */
+
 void print_alphabet_x10(void)
 {
 	char alphabetChar;
 	int multiplicateur;
+
 	for (multiplicateur = 0; multiplicateur <= 9; multiplicateur++)
 	{
 		for (alphabetChar = 'a'; alphabetChar <= 'z'; alphabetChar++)
