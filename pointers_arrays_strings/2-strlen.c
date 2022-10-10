2aca34b2ee991beca326826dcc9fb4c601546b79
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-10 03:41:15 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - check the code;
+ * @s: Take a string as value;
+ * Return: Always 0;
+ */
+
+int _strlen(const char *str)
+{
+	size_t length = strlen(str);
+	return (length);
+}
