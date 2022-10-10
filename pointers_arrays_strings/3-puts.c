8142ef9929e9522e160192ff7f9bfab9ad618c2d
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-10 04:08:42 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the code;
+ * @str: Take a string as value;
+ * Return: Always 0;
+ */
+
+void _puts(const char *str)
+{
+	puts(str);
+}
