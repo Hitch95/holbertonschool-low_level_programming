05d76488872c4e53e2e9110b934f7b35a3eddc62
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-12 04:43:52 -0700

    0. strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,5 +11,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
