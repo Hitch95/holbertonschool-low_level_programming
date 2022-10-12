1cb81559baa38d7b98a6a87ec0c49b6f3550924b
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-12 04:25:04 -0700

    0. strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - check the code, concatenate the strings;
+ * @src: String number one;
+ * @dest: String number two;
+ * Return: Always 0;
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
