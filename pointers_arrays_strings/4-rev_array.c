60e13e1636fe3cee36e03b3429700e54795600d5
Hitch95 <bahloul.condorcet@gmail.com>
2022-10-12 07:30:46 -0700

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 
 /**
+ * swap - create a swap function for use it in reverse_array below;
+ * @y: integer value;
+ * @z: integer value;
+ * Return: Always 0;
+ */
+
+void swap(int *y, int *z)
+{
+	int temp;
+
+	temp = *y;
+	*y = *z;
+	*z = temp;
+}
+
+/**
  * reverse_array - print an array of integers;
  * @a: an array of integers;
  * @n: the number of elements to swap;
- * Return: nothing;
+ * Return: Always 0;
  */
-
-void swap(int *y, int *z){
-    int temp;
-    temp = *y;
-    *y = *z;
-    *z = temp;
-}
 
 void reverse_array(int *a, int n)
 {
